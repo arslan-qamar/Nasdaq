b89db458fb831141f74893df0dbef5104580fde6
Arslan Qamar <ravian720@gmail.com>
2020-10-30 21:03:42 +1100

    Bot is ready

--- start_notifier.py
+++ start_notifier.py
@@ -11,14 +11,14 @@ new_listings = getNewListings(datetime.utcnow().replace(hour=0, minute=0, second
 for new_listing in new_listings:
 
     message =  f"""
-    New Asx Listing : <b>{new_listing['Code']}</b>    
-    Company : <b>{new_listing['Company']}</b> 
-    Price : <b>{new_listing['Price']}</b> 
-    Market Cap : <b>{new_listing['Market_Cap']}</b>    
+    New Nasdaq Listing : <b>{new_listing['ticker']}</b>    
+    Company : <b>{new_listing['company']}</b> 
+    Analyst Consensus : <b>{new_listing['analystConsensus']}</b> 
+    Market Cap : <b>{new_listing['marketCap']/1000000} Million</b> 
+    Market Cap Group : {new_listing['marketCapGroup']} 
+    Sector Name : {new_listing['sectorName']} 
     Discovered at : {new_listing['Discovered_At'].strftime('%d %b %Y %H:%M %p')}      
-    Percentage Change : {new_listing['Percentage_Change']} 
-    Rank : {new_listing['Rank']} 
-    Year Percentage Change : {new_listing['Year_Percentage_Change']}     
+    Price 7 Days : {new_listing['priceChartSevenDay']}
     """
     msg_sent = telegram_bot_sendtext(message)
     print(msg_sent)
